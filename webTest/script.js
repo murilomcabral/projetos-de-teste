8244926c69b0ec655dc9258685fe8ab0555a8c32
murilomcabral <95389598+murilomcabral@users.noreply.github.com>
2022-08-19 07:40:02 -0300

    sticky navbar test

--- webTest/script.js
+++ webTest/script.js
@@ -4,10 +4,9 @@ const pathDoSVG = document.querySelector('svg path');
 const tamanhoTotalDoPath = pathDoSVG.getTotalLenght();
 tagSVG.style.setProperty('--tamanhoTotalDoPath',tamanhoTotalDoPath);
 
-console.log(tamanhoTotalDoPath)
+console.log(tamanhoTotalDoPath);
 
-
-window.addEventListener('scroll', function()) {
-   let navBar = document.querySelector('nav');
+window.addEventListener('scroll', function(){
+   let navBar = document.querySelector('.notSticky');
    navBar.classList.toggle('sticky', window.scrollY > 0);
-}
+})
