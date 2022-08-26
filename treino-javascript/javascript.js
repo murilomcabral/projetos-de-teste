d536be455585738e1d58ed71cd62bff351917f0e
murilomcabral <95389598+murilomcabral@users.noreply.github.com>
2022-08-26 13:37:53 -0300

    testes com menu

--- treino-javascript/javascript.js
+++ treino-javascript/javascript.js
@@ -2,3 +2,11 @@ window.addEventListener('scroll', function () {
    let navA = document.querySelector('body header');
    navA.classList.toggle('sticky', this.scrollY > 0)
 })
+
+function navOpenClose() {
+   if (navBar.style.display == 'none') {
+      navBar.style.display = 'flex';
+   } else {
+      navBar.style.display = 'none';
+   }
+}
