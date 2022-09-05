40df9653c76d6a59eaf4f498b793d505136bdfed
murilomcabral <95389598+murilomcabral@users.noreply.github.com>
2022-09-04 23:58:34 -0300

    tentativas falhas
    common header falho

--- treino-javascript/script/htmlCustom.js
+++ treino-javascript/script/htmlCustom.js
@@ -1,6 +1,38 @@
+// function elementFromHtml(html) {
+//    const template = document.createElement('template');
+//    template.innerHTML = html.trim();
+
+//    return template.content.firstElementChild;
+// }
+
+// const myHeader = elementFromHtml(`
+//        <header  header class="">
+//          <nav>
+//             <ul id="navBar" style="display: none;">
+//                <li><a href="index.html">Início</a></li>
+//                <li><a href="sobre.html">Sobre</a></li>
+//                <li><a href="#">Conhecimentos</a></li>
+//                <li><a href="#">Projetos</a></li>
+//                <li><a href="#">Contato</a></li>
+//             </ul>
+//          </nav>
+//          <div id="navHamb" onclick="navOpenClose()">
+//             <span id="lin1" class=""></span>
+//             <span id="lin2" class=""></span>
+//             <span id="lin3" class=""></span>
+//          </div>
+//          <button id="fontSizeDecrease" onclick="fontDec()">&minus;</button>
+//          <button id="fontSizeNormal" onclick="fontNor()">•</button>
+//          <button id="fontSizeIncrease" onclick="fontInc()">&plus;</button>
+//       </header>
+// `);
+
+// document.body.appendChild(myHeader);
+
 // class myHeader extends HTMLElement {
 //    connectedCallback() { 
-//       this.innerHTML = `<header class="">
+//       this.innerHTML = `
+//       <header class="">
 //       <nav>
 //          <ul id="navBar" style="display: none;">
 //             <li><a href="index.html">Início</a></li>
@@ -18,11 +50,8 @@
 //       <button id="fontSizeDecrease" onclick="fontDec()">&minus;</button>
 //       <button id="fontSizeNormal" onclick="fontNor()">•</button>
 //       <button id="fontSizeIncrease" onclick="fontInc()">&plus;</button>
-//    </header>`
+//    </header>
+//    `
 //    }
 // }
-// customElements.define('myCommonHeader', myHeader)
-
-function mudarUsuario () {
-   prompt();
-}
+// customElements.define('myHeader', myHeader);
