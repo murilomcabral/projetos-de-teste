1ba08d8f9255cbbb259be7f9547306b7c8f7a90b
murilomcabral <95389598+murilomcabral@users.noreply.github.com>
2022-11-14 13:30:49 -0300

    loop

--- webTest/script.js
+++ webTest/script.js
@@ -1,23 +1,33 @@
-
-let machineActive = true;
-let pwd = 'cheese';
-
-let machineResult;
-let pwdResult;
-
-if (machineActive) {
-   machineResult = 'It is ON!'
-   pwd == 'cheese' ? pwdResult = 'You have successfully logged in!' : pwdResult = 'Wrong Password! Try it again.'
-} else {
-   machineResult = 'It is OFF!'
-   pwdResult = ''
+function timesTable(x) {
+   for (let i = 1; i <= 10; i++){
+      console.log(`${i} x ${x} = ${i*x}`)
+   }
 }
 
-// ======================================================
-// ======================================================
+const numTab = 8; //numero a ser calculado
 
-let para1 = machineResult;
-let para2 = pwdResult;
+timesTable(numTab);
+// 1 x 8 = 8
+// 2 x 8 = 16
+// 3 x 8 = 24
+// 4 x 8 = 32
+// 5 x 8 = 40
+// 6 x 8 = 48
+// 7 x 8 = 56
+// 8 x 8 = 64
+// 9 x 8 = 72
+// 10 x 8 = 80
 
-console.log(para1);
-console.log(para2);
+const cats = ['Pete', 'Biggles', 'Jasmine'];
+
+let myFavoriteCats = 'My cats are called ';
+
+for (let i = 0; i < cats.length; i++) {
+  if (i === cats.length - 1) {   // We are at the end of the array
+    myFavoriteCats += `and ${cats[i]}.`
+  } else {
+    myFavoriteCats += `${cats[i]}, `
+  }
+}
+
+console.log(myFavoriteCats);     // "My cats are called Pete, Biggles, and Jasmine."
