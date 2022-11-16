37c515b58d2cd90668609f61cea100f1c8cf4072
murilomcabral <95389598+murilomcabral@users.noreply.github.com>
2022-11-16 00:33:25 -0300

    relogio simples

--- webTest/script.js
+++ webTest/script.js
@@ -1,1 +1,36 @@
-const stringList = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espirito Santo', 'Goiás', 'Maranhão', 'Mato Grosso do Sul', 'Mato Grosso', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins']
+// const caixaHora = document.querySelector('.hora');;
+// const caixaMinuto = document.querySelector('.minuto');
+// const caixaSegundo = document.querySelector('.segundo');
+
+// function timeUpdate() {
+//    let dataHora = new Date();
+//    caixaHora.textContent = `${dataHora.getHours()}`
+//    caixaMinuto.textContent = `${dataHora.getMinutes()}`
+//    caixaSegundo.textContent = `${dataHora.getSeconds()}`
+// }
+
+// setInterval(() => {timeUpdate()}, 100);
+
+function dateTimeUpdate() {
+   let today = new Date();
+   let hora = today.getHours();
+   let minuto = today.getMinutes();
+   let segundo = today.getSeconds();
+
+   if (hora < 10) {
+      hora = '0' + hora;
+   }
+
+   if (minuto < 10) {
+      minuto = '0' + minuto;
+   }
+
+   if (segundo < 10) {
+      segundo = '0' + segundo;
+   }
+
+   let dataHora = [hora,minuto,segundo];
+   let box = document.querySelector('.container p')
+box.textContent = dataHora.join(':')
+}
+setInterval(() => {dateTimeUpdate()}, 100);
