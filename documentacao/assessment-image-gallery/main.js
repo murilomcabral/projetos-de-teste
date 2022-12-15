47b8f8f675a31f7d2c0f1e5ef6dacbe31007b629
murilomcabral <95389598+murilomcabral@users.noreply.github.com>
2022-12-15 01:17:37 -0300

    lets goooooooo

--- documentacao/assessment-image-gallery/main.js
+++ documentacao/assessment-image-gallery/main.js
@@ -5,14 +5,33 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imgFileName = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg']
 
 /* Declaring the alternative text for each image file */
+const altText = ['blurry-sight','desert-sand','purple-flower','egypt-wall-drawing','butterfly-and-flower']
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for (let i = 0; i < imgFileName.length; i++) {
+   const newImage = document.createElement('img');
+   newImage.setAttribute('src', 'images/' + imgFileName[i]);
+   newImage.setAttribute('alt', altText[i]);
+   thumbBar.appendChild(newImage);
+}
 
 /* Wiring up the Darken/Lighten button */
+
+function darkenLighten() {
+   if (btn.innerText === 'Darken') {
+      btn.innerText = 'Lighten';
+      overlay.setAttribute('style','background-color: rgba(0 0 0 / 0.5)');
+   } else {
+      btn.innerText = 'Darken';
+      overlay.setAttribute('style','background-color: rgba(0 0 0 / 0)');
+   }
+}
+
+btn.addEventListener('click', darkenLighten)
+thumbBar.addEventListener('click', function displayImage(event) {
+   displayedImage.setAttribute('src',event.target.getAttribute('src'))
+   displayedImage.setAttribute('alt',event.target.getAttribute('alt'))
+})
