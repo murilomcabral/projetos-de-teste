6a1632fc6d1032995aa550dc9ee679c4c76dcff9
murilomcabral <95389598+murilomcabral@users.noreply.github.com>
2022-12-28 23:35:29 -0300

    Update main.js

--- javascript-mdn/bouncing-balls/main.js
+++ javascript-mdn/bouncing-balls/main.js
@@ -18,3 +18,26 @@ function random(min, max) {
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
+
+// ball object class
+
+class Ball {
+  constructor(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }  
+}
+
+const b = new Ball(200,200,10,5,randomRGB(),200)
+b.draw()
