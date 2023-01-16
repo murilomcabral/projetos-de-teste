629cdba7039dcbc930eeb618611145e933d6ec57
murilomcabral <95389598+murilomcabral@users.noreply.github.com>
2023-01-16 00:49:47 -0300

    age from date

--- treino-javascript/oop/index.js
+++ treino-javascript/oop/index.js
@@ -1,12 +1,42 @@
-const circle = {
-   radius: 1,
-   location: {
-      x: 1,
-      y: 1
-   },
-   draw: function() {
-    console.log('draw')  ;
-   }
-};
+class Person {
 
-circle.draw();
+  name
+  lastname
+  birth_date
+
+  constructor(name, lastname, birth_date) {
+    this.name = name;
+    this.lastname = lastname;
+    this.fullName = name + ' ' + lastname;
+
+    this.birth_date = birth_date;
+
+    this.bDay = this.birth_date.slice(0,2);
+    this.bMonth = this.birth_date.slice(2,4);
+    this.bYear = this.birth_date.slice(4);
+  }
+
+  age() {
+    const date = new Date();
+
+    const acDay = date.getDate();
+    const acMonth = date.getMonth();
+    const acYear = date.getFullYear();
+
+    if(this.birth_date.length !== 8) {
+      return 'ERRO! Data must have two digits for day and month, and four digits for year (fullyear). (e.g., 03101986 results in 03/10/1986)'
+    } else {
+      return `${this.bDay}/${this.bMonth}/${this.bYear}`
+    }
+  }
+
+  greet() {
+    const greetings = `> Hi, ${this.name}!\n  That's what we know about you:\n    >Fullname: ${this.fullName}\n    >Age: ${this.age()}`
+    return greetings
+  }
+
+}
+
+const pessoa1 = new Person('Murilo','Moraes Cabral','01111992')
+
+console.log(pessoa1.greet())
