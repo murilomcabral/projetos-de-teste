5773f3e26e15e8928cc2bad9b3ac8a96bbb8dc20
murilomcabral <95389598+murilomcabral@users.noreply.github.com>
2023-01-31 00:08:54 -0300

    Update script.js

--- javascript-mdn/teste1/script.js
+++ javascript-mdn/teste1/script.js
@@ -1,2 +1,14 @@
-const $container = document.querySelector('.container');
-$container
+// const $container = document.querySelector('.container');
+function largestPrimeFactor(number) {
+  let prime = 2,
+  max = 0;
+  while (prime <= number) {
+    if (number % prime === 0) {
+      number /= prime
+      max = prime
+    } else prime++
+  }
+  return max;
+}
+
+console.log(largestPrimeFactor(13195));
