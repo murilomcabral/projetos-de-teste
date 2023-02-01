5af6ab71147aa7f2be75bab7d2e5e151bf432eb3
murilomcabral <95389598+murilomcabral@users.noreply.github.com>
2023-02-01 00:01:24 -0300

    callback

--- javascript-mdn/teste1/script.js
+++ javascript-mdn/teste1/script.js
@@ -5,4 +5,4 @@ const diff = (arr1, arr2) => [
 
 const sym = (...args) => [...new Set(args.reduce(diff))];
 
-console.log(sym([1, 2, 3], [5, 2, 28, 3, 1, 1, 5 , 3, 4, 3, 4, 7, 0, 1, 4],[2, 7, , 8, 1, 9, 2, 39, 13, 23]).sort((a, b) => a - b));
+console.log(sym([1, 2, 3, 12], [28, 3, 1, 1, 5, 3, 4, 7, 0],[2, 8, 1, 2, 13, 23]).sort((a, b) => a - b));
--- webTest/dom.js
+++ webTest/dom.js
@@ -1,48 +1,20 @@
-let bandInfo;
-
-// =====================
-const favBand = {
-  name:'The Beatles',
-  nationality:'English',
-  genre:'rock',
-  members: 4,
-  formed: 1960,
-  split: 1970,
-  albums: {
-    first: {
-      name: 'Please Please Me',
-      released: 1963
-    },
-    bestseller: {
-      name: 'Abbey Road',
-      released: 1969
-    },
-  },
-  // name:'AC/DC',
-  // nationality:'Australian',
-  // genre:'rock',
-  // members: 5,
-  // formed: 1973,
-  // split: false,
-  // albums: {
-  //   first: {
-  //     name: 'High Voltage',
-  //     released: 1975
-  //   },
-  //   bestseller: {
-  //     name: 'Highway to Hell',
-  //     released: 1980
-  //   },
-  // },
-  info() {
-    const charSelect = this.nationality.charAt(0).toLowerCase();
-    const charString = 'aeiou';
-    return bandInfo = `${this.name} ${this.split == false ? 'are' : 'were'} ${charString.includes(charSelect) ? 'an' : 'a'} ${this.nationality} ${this.genre} band formed in ${this.formed}. ${this.name}\'s first album name was \"${this.albums.first.name}\", released in ${this.albums.first.released}, and the bestseller \"${this.albums.bestseller.name}\", released in ${this.albums.bestseller.released}. ${this.split == false ? `Today, with ${this.members}, they're still on the road` : `They split in ${this.split}, with ${this.members}`}.` ;
-  },
+function createArray(func) {
+  setTimeout(() => {
+    let arr = [],
+    num = 7,
+    i = 1;
+    while (i<=num) {
+      arr.push(i)
+      i++
+    }
+    //func é função que foi passada como parâmetro para a função createArray.
+    func(arr, num)
+  }, 1000);
 }
 
-favBand.info()
-
-// =====================
-
-console.log(bandInfo)
+//função anônima é passada como parâmetro na chamada da função "createArray", a função anônima é passada com dois parâmetros.
+         //|======================função passada como parâmetro======================|  
+createArray(function(array, loopX){console.log(array + ' with ' + loopX + ' loops.')})
+for (let n = 0; n < 10; n++) {
+  console.log('teste1 ' + n)
+}
