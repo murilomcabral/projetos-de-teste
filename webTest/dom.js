dfc93e6837f277c12838965df6a4652011b7163a
murilomcabral <95389598+murilomcabral@users.noreply.github.com>
2023-02-02 00:20:29 -0300

    Update dom.js

--- webTest/dom.js
+++ webTest/dom.js
@@ -1,20 +1,51 @@
-function createArray(func) {
-  setTimeout(() => {
-    let arr = [],
-    num = 7,
-    i = 1;
-    while (i<=num) {
-      arr.push(i)
-      i++
-    }
-    //func é função que foi passada como parâmetro para a função createArray.
-    func(arr, num)
-  }, 1000);
-}
+// function createArray(func) {
+//   setTimeout(() => {
+//     let arr = [],
+//     num = 7,
+//     i = 1;
+//     while (i<=num) {
+//       arr.push(i)
+//       i++
+//     }
+//     //func é função que foi passada como parâmetro para a função createArray.
+//     func(arr, num)
+//   }, 500);
+// }
 
-//função anônima é passada como parâmetro na chamada da função "createArray", a função anônima é passada com dois parâmetros.
-         //|======================função passada como parâmetro======================|  
-createArray(function(array, loopX){console.log(array + ' with ' + loopX + ' loops.')})
-for (let n = 0; n < 10; n++) {
-  console.log('teste1 ' + n)
+// createArray(function(array, loopX){console.log(array + ' with ' + loopX + ' loops.')})
+
+// for (let n = 0; n < 10; n++) {
+//   console.log('teste1 ' + n)
+// }
+
+//=====================================
+//for...in pode trabalhar com objetos
+const arr = [
+  {
+  nome: 'Manoel',
+  idade: 30,
+  pais: 'Brasil'
+  },
+  {
+    nome: 'Edward',
+    idade: 65,
+    pais: 'Brasil'
+  },
+  {
+    nome: 'Gabriel',
+    idade: 27,
+    pais: 'Irlanda'
+  },
+  {
+    nome: 'Jason',
+    idade: 32,
+    pais: 'Brasil'
+  }    
+]
+
+for (let i = 0; i < arr.length; i++) {
+  for (let i2 in arr[i]) {
+    console.log([i2] + ': ' + arr[i][i2])
+  }
+  console.log('=============')
 }
