16688fe92338bfc5b2a88a8ebe773ffcb06a820b
murilomcabral <95389598+murilomcabral@users.noreply.github.com>
2023-05-11 00:01:29 -0300

    primeira etapa - finalizada
    sorteador básico finalizado

--- sorteador/script.js
+++ sorteador/script.js
@@ -1,9 +1,37 @@
-const $textarea = document.querySelector('textarea')
+const $textArea = document.querySelector('textarea')
+const $resultBox = document.querySelector('.result')
+
+$textArea.setAttribute('placeholder', `Exemplo 1:\nMaria,Samanta,Joaquina,Pedro\n\nExemplo 2:\nMaria\nSamanta\nJoaquina\nPedro`)
 
 function delList() {
-  $textarea.value = ''
+  $textArea.value = ''
+  $resultBox.innerHTML = ''
+  $resultBox.style.display = 'none'
 }
 
-function sortear() {
-  return null
+function getList() {
+  let list = []
+  let val = $textArea.value
+
+  if (val != '') {
+    if (val.includes(',')) {
+      list = val.split(',')
+      raffle(list)
+    } else if (val.includes('\n')) {
+      list = val.split('\n')
+      raffle(list)
+    } else {
+      alert('Os valores devem ser separados por vírgula ou quebra de linha!')
+    }
+  }
+}
+
+function raffle(list) {
+  let rnd = Math.floor(Math.random() * (list.length))
+  let winner = list[rnd]
+  $resultBox.style.display = 'block'
+  $resultBox.innerHTML = ''
+  const winAlert = document.createElement('h1')
+  winAlert.innerText = `${winner} ganhou!`
+  $resultBox.appendChild(winAlert)
 }
